90eb11cd2ee6966d3ebd4f65fe65c4a68a0cc9c3
Jack Whiting <hi@jackwhiting.co.uk>
2017-07-14 15:45:33 +0100

    Create searchform.php

---
+++ parts/searchform.php
@@ -0,0 +1,9 @@
+<form action="post" class="autocomplete">
+  <label for="autocomplete">Enter Location</label>
+  <input type="text" id="autocomplete" name="autocomplete" placeholder="Enter Location/Postcode/Area">
+ 
+  <input type="hidden" id="lat" name="lat">
+  <input type="hidden" id="lng" name="lng">
+  
+  <button id="postcode-submit" type="submit" disabled>Search</button>
+</form>
